acc0ba70c3201db3dbb4e2aef1ab64551028041d
jadna <jadna.ac@gmail.com>
2021-04-27 10:23:11 -0300

    ajustando

--- rating.php
+++ rating.php
@@ -50,7 +50,7 @@
     if ($resultado) {
         $msg = "Avaliação salva com sucesso.\nObrigada pela sua participação!\nEntraremos em contato para a próxima etapa da avaliação.";
         echo"<script language='javascript' type='text/javascript'>
-            alert(".$msg.");window.location
+            alert('".$msg."');window.location
             .href='index.php';</script>";
         
         //echo json_encode(['code'=>200, 'msg'=>$msg]);
